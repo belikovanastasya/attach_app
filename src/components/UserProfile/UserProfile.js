51c96d6103757007b6167970499fef3feac1201e
andrey.yatsenko <andrey.yatsenko@codeit.com.ua>
2018-12-27 15:02:39 +0200

    create styles for user page

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -6,30 +6,28 @@ export class UserProfile extends React.Component {
   render() {
     return (
       <form className="user-profile-form">
-      <div className="user-profile_img"><div className="img-holder"><img src={this.props.photo} /></div><input type="file"/></div>
         <div className="user-profile__main">
           <input disabled type="text" name="email" value={this.props.email}/>
+          <button className="edit-btn"><FontAwesomeIcon icon="times" /></button>
           <input
             disabled
             type="text"
             name="firstname"
             placeholder="First Name"
           />
-          <button><FontAwesomeIcon icon="edit" /></button>
+          <button className="edit-btn"><FontAwesomeIcon icon="edit" /></button>
           <input
             disabled
             type="text"
             name="secondname"
             placeholder="Second Name"
           />
-          <button><FontAwesomeIcon icon="edit" /></button>
+          <button className="edit-btn"><FontAwesomeIcon icon="edit" /></button>
           <textarea placeholder="About me" />
-          <button><FontAwesomeIcon icon="edit" /></button>
-          <button className="btn" disabled>
-            Save
-          </button>
+          <button className="edit-btn"><FontAwesomeIcon icon="edit" /></button>
         </div>
-
+        <div className="user-profile_img"><div className="img-holder"><img src={this.props.photo} /></div><input type="file"/></div>
+        <button className="btn">Save</button>
       </form>
     );
   }
