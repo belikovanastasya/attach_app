24d0da5ad8124892e638d18707fbe25bfd374395
andrey.yatsenko <andrey.yatsenko@codeit.com.ua>
2019-01-08 18:03:28 +0200

    fix eslint issues

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -7,8 +7,15 @@ export class Form extends Component {
       { id: 'email', label: 'Email', reg: /^\w+@\w+\.[a-z]{2,}$/ },
       { id: 'firstName', label: 'First name', reg: /^[^ ]{3,20}$/ },
       { id: 'lastName', label: 'Last name', reg: /^[^ ]{3,20}$/ },
-      { id: 'password', label: 'Password', reg: /^[^ ]{6,20}$/, secure: true },
-      { id: 'password_confirm', label: 'Repeat password', reg: /^[^ ]{6,20}$/, secure: true },
+      {
+        id: 'password', label: 'Password', reg: /^[^ ]{6,20}$/, secure: true
+      },
+      {
+        id: 'password_confirm',
+        label: 'Repeat password',
+        reg: /^[^ ]{6,20}$/,
+        secure: true
+      }
     ];
   }
 
@@ -20,18 +27,22 @@ export class Form extends Component {
       error: '',
       checked: false
     };
-    !this.props.data ?
-    this.fields.forEach(field => (this.state[field.id] = { value: '' })) : this.fields.forEach(field => (this.state[field.id] = { value: this.props.data[field.id] }))
+    !this.props.data
+      ? this.fields.forEach(field => (this.state[field.id] = { value: '' }))
+      : this.fields.forEach(
+          field => (this.state[field.id] = { value: this.props.data[field.id] })
+        );
   }
   componentDidMount() {
     this.fields = this.getActualFields();
   }
+
   setValue = ({ target }) => {
     this.setState({
       [target.name]: { value: target.value }
     });
     this.props.clearErrors();
-  }
+  };
   validate = (index) => {
     const field = this.fields[index];
     const stateField = this.state[field.id];
@@ -44,71 +55,77 @@ export class Form extends Component {
     this.setState({
       [field.id]: stateField
     });
-  }
+  };
 
   getDisabledState() {
-    return this.getActualFields()
-      .some(({ id }) => {
-        const { value, error } = this.state[id];
-        return !value || error;
-      });
+    return this.getActualFields().some(({ id }) => {
+      const { value, error } = this.state[id];
+      return !value || error;
+    });
   }
 
-    save = (event) => {
-      const { state } = this;
-      let error = '';
-      event.preventDefault();
-      if (state.password.value !== state.password_confirm.value) {
-        error = 'Passwords should be the same';
-      }
-      this.setState({ error });
-      if (error) return;
-      this.props.onSubmit(this.getFormValue());
+  save = (event) => {
+    const { state } = this;
+    let error = '';
+    event.preventDefault();
+    if (state.password.value !== state.password_confirm.value) {
+      error = 'Passwords should be the same';
     }
-    getFormValue() {
-      const form = {};
-      this.fields
-        .filter(field => !this.props.excluded.includes(field.id))
-        .filter(field => !this.props.skipped.includes(field.id))
-        .forEach(field => form[field.id] = this.state[field.id].value);
-      return form;
-    }
-    getActualFields() {
-      return this.fields
-        .filter(field => !this.props.excluded.includes(field.id))
-        .filter(field => !this.props.skipped.includes(field.id))
-        .filter(field => !this.props.disabled.includes(field.id));
-    }
-    render() {
-      const { state, fields } = this;
-      const { excluded, disabled, buttonName, checkbox, desctiption, avatar } = this.props;
-      const buttons = { signUp: { name: 'sing Up', value: 'Sing Up' }, save: { name: 'Save', value: 'Save' } };
-      return (
-        <form
-          className="form"
-          onSubmit={this.save}
-        >
-          <div className="registration-template none-aithorize-template active">
-          {avatar && <div className="additional-field">
-          <div className="avatar-holder">
-          <input
-            name='avatar'
-            type='file'
-            onChange={this.setValue}
-          />
-          </div>
-          </div>}
-        {checkbox && <div className="additional-field">
-          <div className="checkbox-holder">
-          <input
-            name='checked'
-            type='checkbox'
-            checked={state.checked}
-            onChange={this.setValue}
-          />
-          </div>
-          <span>I am a desinger</span>
-          </div>}
+    this.setState({ error });
+    if (error) return;
+    this.props.onSubmit(this.getFormValue());
+  };
+  getFormValue() {
+    const form = {};
+    this.fields
+      .filter(field => !this.props.excluded.includes(field.id))
+      .filter(field => !this.props.skipped.includes(field.id))
+      .forEach(field => (form[field.id] = this.state[field.id].value));
+    return form;
+  }
+  getActualFields() {
+    return this.fields
+      .filter(field => !this.props.excluded.includes(field.id))
+      .filter(field => !this.props.skipped.includes(field.id))
+      .filter(field => !this.props.disabled.includes(field.id));
+  }
+  render() {
+    const { state, fields } = this;
+    const {
+      excluded,
+      disabled,
+      buttonName,
+      checkbox,
+      desctiption,
+      avatar
+    } = this.props;
+    const buttons = {
+      signUp: { name: 'sing Up', value: 'Sing Up' },
+      save: { name: 'Save', value: 'Save' }
+    };
+    return (
+      <form className="form" onSubmit={this.save}>
+        <div className="registration-template none-aithorize-template active">
+          {avatar && (
+            <div className="additional-field">
+              <div className="avatar-holder">
+                <input name="avatar" type="file" onChange={this.setValue} />
+              </div>
+            </div>
+          )}
+          {checkbox && (
+            <div className="additional-field">
+              <div className="checkbox-holder">
+                <input
+                  name="checked"
+                  type="checkbox"
+                  checked={state.checked}
+                  onChange={this.setValue}
+                />
+              </div>
+              <span>I am a desinger</span>
+            </div>
+          )}
           {fields
             .filter(({ id }) => !excluded.includes(id))
             .map(({ label, secure, id }, index) => {
@@ -125,27 +142,29 @@ export class Form extends Component {
                     onBlur={() => this.validate(index)}
                     disabled={disabled.includes(id)}
                   />
-                  {stateField.error && <span className="error-text">{stateField.error}</span>}
+                  {stateField.error && (
+                    <span className="error-text">{stateField.error}</span>
+                  )}
                 </div>
               );
             })}
-            {desctiption && <textarea
-            name='description'
-            onChange={this.setValue}
-           />}
-          </div>
-          {state.error && <span className="error-text">{state.error}</span>}
-          <div className="btn_holder">
-            <button
-              className="saveBtn btn"
-              type="submit"
-              disabled={this.getDisabledState()}
-            ><span>{buttons[buttonName].name}</span>
-            </button>
-          </div>
-        </form>
-      );
-    }
+          {desctiption && (
+            <textarea name="description" onChange={this.setValue} />
+          )}
+        </div>
+        {state.error && <span className="error-text">{state.error}</span>}
+        <div className="btn_holder">
+          <button
+            className="saveBtn btn"
+            type="submit"
+            disabled={this.getDisabledState()}
+          >
+            <span>{buttons[buttonName].name}</span>
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
 
 Form.defaultProps = {
--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,31 +1,30 @@
+import { React } from 'react';
 import { Form } from '../Form';
 import './userprofile.sass';
 
 
 export class UserProfile extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  saveChanges = (user) => {
-    console.log(user)
-  }
+
+  saveChanges = user => {
+    console.log(user);
+  };
   render() {
-    const {user} = this.props;
+    const { user } = this.props;
     return (
-        <React.Fragment>
+      <React.Fragment>
         <div className="user-profile__main">
-        <Form
-              excluded={['password', 'password_confirm']}
-              onSubmit={this.saveChanges}
-              buttonName="save"
-              checkbox={true}
-              desctiption={true}
-              avatar={true}
-              data={user}
-            />
+          <Form
+            excluded={['password', 'password_confirm']}
+            onSubmit={this.saveChanges}
+            buttonName="save"
+            checkbox
+            desctiption
+            avatar
+            data={user}
+          />
         </div>
         {/* <div className="user-profile_img"><div className="img-holder"><img src={this.props.photo} /></div><input type="file"/></div> */}
-        </React.Fragment>
+      </React.Fragment>
     );
   }
 }
