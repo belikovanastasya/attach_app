cb2b4150078e62991753210b0ec87fd0d9dfcc62
belikova <belikova.ava@gmail.com>
2019-01-16 15:49:40 +0200

    create ueser work list

--- src/components/UserWorksListItem/UserWorksListItem.js
+++ src/components/UserWorksListItem/UserWorksListItem.js
@@ -1,1 +1,14 @@
 import './userWorksListItem.sass';
+
+export const UserWorkListItem = (props) => {
+  const { work } = props;
+  return (
+    <div>
+      <img src={work.images.sketch} alt="sketch" />
+      <h2>{work.name}</h2>
+      <span>Project</span>
+      <span>{work.project}</span>
+      <span>Votes</span><span>{work.votes}</span>
+    </div>
+  );
+};
--- src/components/UserWorkstList/UserWorksList.js
+++ src/components/UserWorkstList/UserWorksList.js
@@ -1,17 +1,23 @@
 import './userWorksList.sass';
+import { UserWorksListItem } from '../UserWorksListItem';
 import { getUserWorks } from '../../servises/users';
 
 
 export class UserWorksList extends React.Component {
-
+  constructor(){
+    super();
+  }
   componentDidMount() {
-    getUserWorks(this.props.user).then(works => this.setState(works));
+    getUserWorks(this.props.user).then(({works}) => this.setState({works: works}));
   }
 
   render() {
     const { state } = this;
+    console.log(this.state)
     return (
-      <div />
+      <div className="user-profile__works">
+        {/* {state.works.map((index, item) => (<UserWorksListItem key={index} work={item} />))} */}
+      </div>
     );
   }
 }
