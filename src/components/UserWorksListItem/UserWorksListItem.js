f779d1685df167b5b2eb09e9be53f98b30d21925
belikovanastasya <belikova.ava@gmail.com>
2019-01-20 22:19:52 +0200

    markup for user work component

--- src/components/UserWorksListItem/UserWorksListItem.js
+++ src/components/UserWorksListItem/UserWorksListItem.js
@@ -1,13 +1,32 @@
 import './userWorksListItem.sass';
 
-export const UserWorksListItem = (props) => {
-  const { works } = props;
-  return (
-    <div className="work__item">
-      <h2>{works.name}</h2>
-      <span>Project</span>
-      <span>{works.project}</span>
-      <span>Votes</span><span>{works.votes}</span>
-    </div>
-  );
-};
+
+export class UserWorksListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: true
+    };
+  }
+  render() {
+    const { works } = this.props;
+    const { open } = this.state;
+    return (
+      <div className="user-profile-works__item">
+        <h2>{works.name}</h2>
+        <div>
+          <span className="row_point">Project:</span>
+          <span>{works.project}</span>
+        </div>
+        <div>
+          <span className="row_point">Votes:</span>
+          <span>{works.vots}</span>
+        </div>
+        <div className="user-profile-works__btn">{open
+            ? 'More'
+          : 'Close'}
+        </div>
+      </div>
+    );
+  }
+}
